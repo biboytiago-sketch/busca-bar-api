267fc5567705f922dfa638034d31c7b680737a0a
Novo Usuário <novousuario@Novos-MacBook-Pro.local>
2026-06-25 14:42:17 -0300

    correcao definitiva do app.js com pasta public

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
 const express = require('express');
-const cors = require('cors');
-const apiRoutes = require('./src/routes/api'); // Como o app saiu da src, agora precisa do './src/' aqui
-require('dotenv').config();
-
+const path = require('path');
 const app = express();
 
-app.use(cors());
-app.use(express.json());
+const BarController = require('./src/controllers/BarController');
+const PromocaoController = require('./src/controllers/PromocaoController');
 
-app.use('/api', apiRoutes);
+// 1. Configurações Iniciais do Servidor
+app.use(express.json()); // Permite ler JSON
+
+// 2. LIBERAÇÃO DAS TELAS HTML (Não delete esta linha!)
+app.use(express.static(path.join(__dirname, 'public')));
+
+// 3. --- ROTAS DO GERENTE (ABA ADM) ---
+app.post('/api/bares', BarController.cadastrar);
+app.post('/api/promocoes', PromocaoController.criar);
+
+// 4. --- ROTAS DO CLIENTE (APLICATIVO) ---
+app.get('/api/bares', BarController.listar);
+app.get('/api/promocoes', PromocaoController.listarAtivas);
+app.get('/api/promocoes/categoria/:categoria', PromocaoController.filtrarPorCategoria);
 
 module.exports = app;
